51515dfb52555306a745b0250e9d0bdc18a7b9fb
Artem Polisskyi <polesskiy.dev@gmail.com>
2016-09-30 12:06:07 +0300

    start add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,10 @@ module.exports = {
                     'css?modules&importLoaders=1&localIdentName=[name]---[local]---[hash:base64:5]',
                     'autoprefixer',
                     'less')
-            }
+            },
+            //webfonts
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
         ]
     },
 
